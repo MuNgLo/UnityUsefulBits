47c4b5ef05c4c5d4bde2f3f2d19c0aa3d89e486d
MuNgLo <kvarnrud@gmail.com>
2018-08-10 19:50:51 +0200

    added CursorToggler

---
+++ SingleSimpleScripts/MousePointerToggler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePointerToggler : MonoBehaviour {
+    public KeyCode _key;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(_key))
+        {
+            if (Cursor.visible)
+            {
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+            else
+            {
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+            }
+        }
+	}
+}
