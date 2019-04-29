a9471599f1207d4ab0ccea01c7ff993b30aa04fd
MuNgLo <kvarnrud@gmail.com>
2019-04-29 21:33:40 +0200

    Blender FBX Material order notes and LookAtObject added

---
+++ SingleSimpleScripts/LookAtObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtObject : MonoBehaviour
+{
+    public GameObject target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target)
+        {
+            this.transform.LookAt(target.transform.position);
+        }
+    }
+}
